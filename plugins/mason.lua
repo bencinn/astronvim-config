931eb4ee1547ee5e2737a17facaa4af4e9b4d72a
hansnnn <pp.rongpan@gmail.com>
2023-04-10 09:39:15 +0700

    initial commit

---
+++ plugins/mason.lua
@@ -0,0 +1,50 @@
+-- customize mason plugins
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
+    },
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      ensure_installed = { "prettier", "stylua", "rustfmt" },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    config = function(plugin, opts)
+      local mason_nvim_dap = require "mason-nvim-dap"
+      mason_nvim_dap.setup(opts) -- run setup
+      -- do more configuration as needed
+      mason_nvim_dap.setup_handlers {
+        python = function(source_name)
+          local dap = require "dap"
+          dap.adapters.python = {
+            type = "executable",
+            command = "/usr/bin/python3",
+            args = {
+              "-m",
+              "debugpy.adapter",
+            },
+          }
+
+          dap.configurations.python = {
+            {
+              type = "python",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}", -- This configuration will launch the current file if used.
+            },
+          }
+        end,
+      }
+    end,
+  },
+}
---
+++ plugins/user.lua
@@ -0,0 +1,66 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+    },
+    init = function()
+      require("noice").setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+          hover = {
+            enabled = false,
+          },
+          signature = {
+            enabled = false,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
+  { "echasnovski/mini.animate", version = false, init = function() require("mini.animate").setup() end },
+  {
+    "ggandor/leap.nvim",
+    dependencies = { "tpope/vim-repeat" },
+    init = function() require("leap").add_default_mappings() end,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    init = function() require("transparent").setup() end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = true,
+    init = function()
+      require("notify").setup {
+        background_colour = "#000000",
+      }
+    end,
+  },
+}
